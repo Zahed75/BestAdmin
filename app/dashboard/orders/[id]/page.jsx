e7ba82fab0229e7c31e134fde2da9cc0da39d390
shaiadul <devshaiadul69@gmail.com>
2024-06-22 10:31:42 +0600

    order single page

--- app/dashboard/orders/[id]/page.jsx
+++ app/dashboard/orders/[id]/page.jsx
@@ -1,6 +1,22 @@
 import DynamicHead from "@/components/dashboard/orderpage/dynamic/DynamicHead";
 
-export default function Page() {
+export default function Page({params}) {
+  const id = params.id;
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchApi(`/product/getProductById/${id}`, "GET");
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching product data:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
   return (
     <main className="">
       {/* top section */}
