506e6acad15ee0bffb61b4e2de8415a0a7e3eef1
shaiadul <devshaiadul69@gmail.com>
2024-08-20 15:16:08 +0600

    pull update

--- app/dashboard/settings/events/EventsPage.jsx
+++ app/dashboard/settings/events/EventsPage.jsx
@@ -35,7 +35,7 @@ export default function EventsPage({ initialItems }) {
       // setItems(initialOrderedItems);
     }
     // }, [initialItems]);
-  });
+  }, []);
 
   const [draggedItem, setDraggedItem] = useState(null);
 
@@ -123,15 +123,15 @@ export default function EventsPage({ initialItems }) {
                   <div className="text-sm text-white bg-black rounded-full px-2">2 x 3</div>
                   <div className="flex flex-row gap-x-2">
                     <svg className="cursor-pointer" onClick={() => setShowUpdateMenu(true)} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M16.2141 4.98239L17.6158 3.58063C18.39 2.80646 19.6452 2.80646 20.4194 3.58063C21.1935 4.3548 21.1935 5.60998 20.4194 6.38415L19.0176 7.78591M16.2141 4.98239L10.9802 10.2163C9.93493 11.2616 9.41226 11.7842 9.05637 12.4211C8.70047 13.058 8.3424 14.5619 8 16C9.43809 15.6576 10.942 15.2995 11.5789 14.9436C12.2158 14.5877 12.7384 14.0651 13.7837 13.0198L19.0176 7.78591M16.2141 4.98239L19.0176 7.78591" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                      <path d="M21 12C21 16.2426 21 18.364 19.682 19.682C18.364 21 16.2426 21 12 21C7.75736 21 5.63604 21 4.31802 19.682C3 18.364 3 16.2426 3 12C3 7.75736 3 5.63604 4.31802 4.31802C5.63604 3 7.75736 3 12 3" stroke="black" stroke-width="1.5" stroke-linecap="round" />
+                      <path d="M16.2141 4.98239L17.6158 3.58063C18.39 2.80646 19.6452 2.80646 20.4194 3.58063C21.1935 4.3548 21.1935 5.60998 20.4194 6.38415L19.0176 7.78591M16.2141 4.98239L10.9802 10.2163C9.93493 11.2616 9.41226 11.7842 9.05637 12.4211C8.70047 13.058 8.3424 14.5619 8 16C9.43809 15.6576 10.942 15.2995 11.5789 14.9436C12.2158 14.5877 12.7384 14.0651 13.7837 13.0198L19.0176 7.78591M16.2141 4.98239L19.0176 7.78591" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                      <path d="M21 12C21 16.2426 21 18.364 19.682 19.682C18.364 21 16.2426 21 12 21C7.75736 21 5.63604 21 4.31802 19.682C3 18.364 3 16.2426 3 12C3 7.75736 3 5.63604 4.31802 4.31802C5.63604 3 7.75736 3 12 3" stroke="black" strokeWidth="1.5" strokeLinecap="round" />
                     </svg>
 
                     <svg className="cursor-pointer" onClick={() => setShowUpdateMenu(true)} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5" stroke="#FF3B30" stroke-width="1.5" stroke-linecap="round" />
-                      <path d="M3 5.5H21M16.0557 5.5L15.3731 4.09173C14.9196 3.15626 14.6928 2.68852 14.3017 2.39681C14.215 2.3321 14.1231 2.27454 14.027 2.2247C13.5939 2 13.0741 2 12.0345 2C10.9688 2 10.436 2 9.99568 2.23412C9.8981 2.28601 9.80498 2.3459 9.71729 2.41317C9.32164 2.7167 9.10063 3.20155 8.65861 4.17126L8.05292 5.5" stroke="#FF3B30" stroke-width="1.5" stroke-linecap="round" />
-                      <path d="M9.5 16.5V10.5" stroke="#FF3B30" stroke-width="1.5" stroke-linecap="round" />
-                      <path d="M14.5 16.5V10.5" stroke="#FF3B30" stroke-width="1.5" stroke-linecap="round" />
+                      <path d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5" stroke="#FF3B30" strokeWidth="1.5" strokeLinecap="round" />
+                      <path d="M3 5.5H21M16.0557 5.5L15.3731 4.09173C14.9196 3.15626 14.6928 2.68852 14.3017 2.39681C14.215 2.3321 14.1231 2.27454 14.027 2.2247C13.5939 2 13.0741 2 12.0345 2C10.9688 2 10.436 2 9.99568 2.23412C9.8981 2.28601 9.80498 2.3459 9.71729 2.41317C9.32164 2.7167 9.10063 3.20155 8.65861 4.17126L8.05292 5.5" stroke="#FF3B30" strokeWidth="1.5" strokeLinecap="round" />
+                      <path d="M9.5 16.5V10.5" stroke="#FF3B30" strokeWidth="1.5" strokeLinecap="round" />
+                      <path d="M14.5 16.5V10.5" stroke="#FF3B30" strokeWidth="1.5" strokeLinecap="round" />
                     </svg>
                   </div>
                 </div>
@@ -193,8 +193,8 @@ export default function EventsPage({ initialItems }) {
                               </div>
                             </Link>
                             <div className="mt-5 flex justify-start items-center">
-                              <p class="text-[#70BE38] text-xs font-semibold border border-[#70BE38] rounded-md px-3 py-1">In Stock</p>
-                              <span class="text-[#F16521] text-xs font-semibold ml-3 px-3 py-1 border border-[#F16521] rounded-md">Online &amp; Offline</span>
+                              <p className="text-[#70BE38] text-xs font-semibold border border-[#70BE38] rounded-md px-3 py-1">In Stock</p>
+                              <span className="text-[#F16521] text-xs font-semibold ml-3 px-3 py-1 border border-[#F16521] rounded-md">Online &amp; Offline</span>
                             </div>
                             <div className="mt-3">
                               <Link href="">
@@ -294,8 +294,8 @@ export default function EventsPage({ initialItems }) {
                               </div>
                             </Link>
                             <div className="mt-5 flex justify-start items-center">
-                              <p class="text-[#70BE38] text-xs font-semibold border border-[#70BE38] rounded-md px-3 py-1">In Stock</p>
-                              <span class="text-[#F16521] text-xs font-semibold ml-3 px-3 py-1 border border-[#F16521] rounded-md">Online &amp; Offline</span>
+                              <p className="text-[#70BE38] text-xs font-semibold border border-[#70BE38] rounded-md px-3 py-1">In Stock</p>
+                              <span className="text-[#F16521] text-xs font-semibold ml-3 px-3 py-1 border border-[#F16521] rounded-md">Online &amp; Offline</span>
                             </div>
                             <div className="mt-3">
                               <Link href="">
@@ -395,8 +395,8 @@ export default function EventsPage({ initialItems }) {
                               </div>
                             </Link>
                             <div className="mt-5 flex justify-start items-center">
-                              <p class="text-[#70BE38] text-xs font-semibold border border-[#70BE38] rounded-md px-3 py-1">In Stock</p>
-                              <span class="text-[#F16521] text-xs font-semibold ml-3 px-3 py-1 border border-[#F16521] rounded-md">Online &amp; Offline</span>
+                              <p className="text-[#70BE38] text-xs font-semibold border border-[#70BE38] rounded-md px-3 py-1">In Stock</p>
+                              <span className="text-[#F16521] text-xs font-semibold ml-3 px-3 py-1 border border-[#F16521] rounded-md">Online &amp; Offline</span>
                             </div>
                             <div className="mt-3">
                               <Link href="">
@@ -724,7 +724,7 @@ export default function EventsPage({ initialItems }) {
                   <div className="flex flex-col space-y-1 w-full">
                     <label className="inline-flex items-center cursor-pointer gap-2">
                       Hide Out of Stock
-                      <input type="checkbox" class="sr-only peer" />
+                      <input type="checkbox" className="sr-only peer" />
                       <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
 
                     </label>
