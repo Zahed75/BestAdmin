16d483d834c40554b50c36124f56446bbca3a70c
shaiadul <devshaiadul69@gmail.com>
2024-09-24 18:27:10 +0600

    ImageUpload: single image upload

--- components/global/modal/ImageUploadModal .jsx
+++ components/global/modal/ImageUploadModal .jsx
@@ -7,9 +7,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { addImage, removeImage } from "@/redux/slice/imagesSlice";
 
 const ImageUploadModal = ({ isOpen, onClose }) => {
-  const [activeTab, setActiveTab] = useState("upload");
-  const [featureFile, setFeatureFile] = useState(null);
-  const [productFile, setProductFile] = useState(null);
+  // const [activeTab, setActiveTab] = useState("upload");
+  const [imageFile, setImageFile] = useState(null);
+  const [imgList, setImgList] = useState([]);
   const [allImages, setAllImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -20,114 +20,47 @@ const ImageUploadModal = ({ isOpen, onClose }) => {
     uploadProgress,
     error: uploadError,
     successMessage,
+    activeTab,
+    setActiveTab,
   } = useImageUpload();
 
   const dispatch = useDispatch();
   const selectedImages = useSelector((state) => state.images.selectedImages);
-  
 
   useEffect(() => {
-    if (isOpen) {
-      fetchImages();
-    }
-  }, [isOpen]);
+    const fetchImages = async () => {
+      try {
+        const response = await axios.get(
+          "https://service.bestelectronics.com.bd/feature/api/get-all-images/"
+        );
+        setImgList(response?.data?.data);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+        setError("Error fetching images.");
+      }
+    };
 
-  const fetchImages = async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await axios.get("https://via.placeholder.com/300x200"); // Replace with your API URL
-      setAllImages(response.data);
-    } catch (err) {
-      setError("Failed to fetch images. Please try again.");
-    } finally {
-      setIsLoading(false);
-    }
+    fetchImages();
+  }, [activeTab]);
+
+  const handleImageFileChange = (e) => {
+    setImageFile(e.target.files[0]);
   };
 
-  const handleFeatureFileChange = (e) => {
-    setFeatureFile(e.target.files[0]);
-  };
-
-  const handleProductFileChange = (e) => {
-    setProductFile(e.target.files[0]);
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    uploadImage(featureFile, productFile);
+    if (imageFile) {
+      const response = await uploadImage(imageFile, "images", "product images");
+    }
   };
-
-  // const handleImageSelect = (image) => {
-  //   if (selectedImages.includes(image.src)) {
-  //     setSelectedImages(selectedImages.filter((img) => img !== image.src));
-  //   } else {
-  //     setSelectedImages([...selectedImages, image.src]);
-  //   }
-  // };
 
   const handleImageSelect = (image) => {
-    if (selectedImages.includes(image.src)) {
-      dispatch(removeImage(image.src));
+    if (selectedImages.includes(image.ImageUrl)) {
+      dispatch(removeImage(image.ImageUrl));
     } else {
-      dispatch(addImage(image.src));
+      dispatch(addImage(image.ImageUrl));
     }
   };
-
-  const imgList = [
-    {
-      id: 1,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 2,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 3,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 4,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 5,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 6,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 7,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 8,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 9,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-    {
-      id: 10,
-      src: "https://via.placeholder.com/300x200",
-      selected: false,
-    },
-  ];
-
-  console.log("Selected Images: ", selectedImages);
 
   return (
     <>
@@ -136,7 +69,7 @@ const ImageUploadModal = ({ isOpen, onClose }) => {
           <div className="bg-white rounded-lg p-6 w-full max-w-4xl shadow-lg overflow-y-auto max-h-[80vh] md:mx-10 mx-4 lg:max-w-6xl lg:px-10 scrollbar_hidden">
             <div className="flex justify-between items-center mb-6">
               <h2 className="text-xl font-bold text-gray-700">
-                Upload or Select Images
+                Upload or Select One Image
               </h2>
               <button
                 onClick={onClose}
@@ -198,6 +131,7 @@ const ImageUploadModal = ({ isOpen, onClose }) => {
                           name="file-upload"
                           type="file"
                           className="sr-only"
+                          onChange={handleImageFileChange}
                         />
                       </label>
                     </h3>
@@ -206,13 +140,6 @@ const ImageUploadModal = ({ isOpen, onClose }) => {
                     </p>
                   </div>
                 </div>
-                <button
-                  type="submit"
-                  disabled={isUploading}
-                  className="bg-[#f26522] text-white text-lg py-2 px-4 rounded-lg mt-4 focus:outline-none transition ease-in-out duration-200 flex ml-auto"
-                >
-                  {isUploading ? "Uploading..." : "Upload Images"}
-                </button>
 
                 {isUploading && (
                   <p className="mt-4 text-gray-600">
@@ -225,6 +152,14 @@ const ImageUploadModal = ({ isOpen, onClose }) => {
                 {successMessage && (
                   <p className="mt-4 text-green-500">{successMessage}</p>
                 )}
+
+                <button
+                  type="submit"
+                  disabled={isUploading}
+                  className="bg-[#f26522] text-white text-lg py-2 px-4 rounded-lg focus:outline-none transition ease-in-out duration-200 flex ml-auto"
+                >
+                  {isUploading ? "Uploading..." : "Upload Images"}
+                </button>
               </form>
             )}
 
@@ -232,26 +167,31 @@ const ImageUploadModal = ({ isOpen, onClose }) => {
               <div
                 className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-y-auto min-h-[60vh] scrollbar_hidden`}
               >
-                {imgList.map((image) => (
-                  <div key={image.id} className="relative">
-                    <img
-                      src={image.src}
-                      alt="Uploaded"
-                      className={`w-full h-32 object-cover rounded-lg shadow-sm cursor-pointer ${
-                        selectedImages.includes(image.src)
-                          ? "border-2 border-[#f26522]"
-                          : ""
-                      }`}
-                      onClick={() => handleImageSelect(image)}
-                    />
+                {imgList
+                  .slice()
+                  .reverse()
+                  .map((image, i) => (
+                    <div key={i} className="relative">
+                      <Image
+                        width={100}
+                        height={100}
+                        src={image.ImageUrl}
+                        alt="Uploaded"
+                        className={`w-full h-32 object-cover rounded-lg shadow-sm cursor-pointer ${
+                          selectedImages.includes(image.ImageUrl)
+                            ? "border-2 border-[#f26522]"
+                            : ""
+                        }`}
+                        onClick={() => handleImageSelect(image)}
+                      />
 
-                    {selectedImages.includes(image.src) && (
-                      <div className="absolute top-0 right-0 bg-[#f26522] text-white p-1 h-6 w-6 flex items-center justify-center rounded-full">
-                        <span className="text-sm">✔</span>
-                      </div>
-                    )}
-                  </div>
-                ))}
+                      {selectedImages.includes(image.ImageUrl) && (
+                        <div className="absolute top-0 right-0 bg-[#f26522] text-white p-1 h-6 w-6 flex items-center justify-center rounded-full">
+                          <span className="text-sm">✔</span>
+                        </div>
+                      )}
+                    </div>
+                  ))}
               </div>
             )}
           </div>
