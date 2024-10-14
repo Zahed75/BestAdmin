4a2a8b52309d018dd5009c56943c55cfd554a7ce
MINHAJUL ISLAM <minhajul_islam1@hotmail.com>
2024-10-14 16:27:35 +0600

    Inventory and Product Modal Functional

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -3,31 +3,40 @@ import ProductTable from "@/components/dashboard/productpage/ProductTable";
 import PageHead from "@/components/global/pageHead/PageHead";
 import Skeleton from "@/components/global/skeleton/Skeleton";
 import { fetchProducts } from "@/redux/slice/productsSlice";
+import { fetchOutlets } from "@/redux/slice/outletSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function ProductsPage() {
   const dispatch = useDispatch();
   const product = useSelector((state) => state?.products);
+  const outlets = useSelector((state) => state?.outlets?.outlets?.outlet);
 
   useEffect(() => {
     dispatch(fetchProducts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchOutlets());
   }, [dispatch]);
 
   const AllProducts = product?.products?.products
     ? [...product.products.products].reverse()
     : [];
 
-  const data = AllProducts || [];
+  const AllOutlets = outlets || [];
+
+  const data_product = AllProducts || [];
+  const data_outlets = AllOutlets || [];
 
   return (
     <main>
-      {data.length === 0 ? (
+      {data_product.length === 0 ? (
         <Skeleton />
       ) : (
         <div>
           <PageHead pageHead="Products" />
-          <ProductTable AllProducts={AllProducts} />
+          <ProductTable AllProducts={AllProducts} AllOutlets={AllOutlets} />
         </div>
       )}
     </main>
