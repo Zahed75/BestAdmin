5df11d330c32c78179762c22a83529053a9c91cd
MINHAJUL ISLAM <minhajul_islam1@hotmail.com>
2024-10-20 14:48:20 +0600

    added Delete Icon

--- components/dashboard/outletspage/dynamic/InventoryTable.jsx
+++ components/dashboard/outletspage/dynamic/InventoryTable.jsx
@@ -255,9 +255,8 @@ export default function InventoryTable() {
                     {currentData?.map((item) => (
                       <tr
                         key={item.id}
-                        className={`${
-                          item.id % 2 !== 0 ? "" : "bg-gray-100"
-                        } hover:bg-gray-100 duration-700`}
+                        className={`${item.id % 2 !== 0 ? "" : "bg-gray-100"
+                          } hover:bg-gray-100 duration-700`}
                       >
                         <td scope="col" className="p-4">
                           <div className="flex items-center">
@@ -305,11 +304,10 @@ export default function InventoryTable() {
                             className={`${item.bg} ${item.text} px-2 py-1 rounded-full`}
                           > */}
                           <div
-                            className={`${
-                              item.stock === "In Stock"
-                                ? "bg-green-100 text-green-400"
-                                : "bg-red-100 text-red-400"
-                            } inline-block px-1 py-1 rounded-md mr-2 `}
+                            className={`${item.stock === "In Stock"
+                              ? "bg-green-100 text-green-400"
+                              : "bg-red-100 text-red-400"
+                              } inline-block px-1 py-1 rounded-md mr-2 `}
                           >
                             <div className="flex justify-center px-1 space-x-2">
                               {/* <div><span>{item.stock}({item.stockQuantity || 0})</span></div> */}
@@ -364,9 +362,42 @@ export default function InventoryTable() {
                         </td>
                         <td className="px-6 lg:px-0 py-4 text-[12px] font-medium  whitespace-nowrap ">
                           <button
-                            className={` px-2 py-1 rounded-md border border-black`}
+                            className={` px-2 py-1 rounded-md borde`}
                           >
-                            View Details
+                            <svg
+                              className="cursor-pointer"
+                              // onClick={() => handleDeleteGrid(item._id)}
+                              width="22"
+                              height="22"
+                              viewBox="0 0 22 22"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M19.5 5.5L18.8803 15.5251C18.7219 18.0864 18.6428 19.3671 18.0008 20.2879C17.6833 20.7431 17.2747 21.1273 16.8007 21.416C15.8421 22 14.559 22 11.9927 22C9.42312 22 8.1383 22 7.17905 21.4149C6.7048 21.1257 6.296 20.7408 5.97868 20.2848C5.33688 19.3626 5.25945 18.0801 5.10461 15.5152L4.5 5.5"
+                                stroke="#FF3B30"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                              />
+                              <path
+                                d="M3 5.5H21M16.0557 5.5L15.3731 4.09173C14.9196 3.15626 14.6928 2.68852 14.3017 2.39681C14.215 2.3321 14.1231 2.27454 14.027 2.2247C13.5939 2 13.0741 2 12.0345 2C10.9688 2 10.436 2 9.99568 2.23412C9.8981 2.28601 9.80498 2.3459 9.71729 2.41317C9.32164 2.7167 9.10063 3.20155 8.65861 4.17126L8.05292 5.5"
+                                stroke="#FF3B30"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                              />
+                              <path
+                                d="M9.5 16.5V10.5"
+                                stroke="#FF3B30"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                              />
+                              <path
+                                d="M14.5 16.5V10.5"
+                                stroke="#FF3B30"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                              />
+                            </svg>
                           </button>
                         </td>
                       </tr>
@@ -447,9 +478,8 @@ export default function InventoryTable() {
       <Modal addModal={() => setShowAddMenu(false)}>
         <div
           id="menu"
-          className={`w-full h-full bg-gray-900 bg-opacity-80 top-0 right-0 ${
-            showAddMenu ? "fixed" : "hidden"
-          } sticky-0 z-30`}
+          className={`w-full h-full bg-gray-900 bg-opacity-80 top-0 right-0 ${showAddMenu ? "fixed" : "hidden"
+            } sticky-0 z-30`}
         >
           <div className="flex justify-center items-center min-h-screen px-4 sm:px-6 lg:px-8">
             <form
@@ -522,9 +552,8 @@ export default function InventoryTable() {
                     {/* Dropdown arrow */}
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                       <svg
-                        className={`w-5 h-5 text-gray-400 transition-transform duration-300 ${
-                          isOpen ? "rotate-180" : ""
-                        }`}
+                        className={`w-5 h-5 text-gray-400 transition-transform duration-300 ${isOpen ? "rotate-180" : ""
+                          }`}
                         viewBox="0 0 20 20"
                         fill="currentColor"
                       >
